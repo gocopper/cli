f6410be9cd3f17fbc523b2d688a743d21fb1b13a
Tushar Soni <tusharsoni1205@gmail.com>
2025-07-19 14:26:20 -0400

    Update template dependencies and refactor frontendnone modifier
    - Update copper dependency to v0.7.4-0.20250305165433-a5bcd6e46fac
    - Refactor frontendnone modifier to use embedded templates
    - Remove hardcoded route creation in favor of template-based approach

--- cmd/copper/web/frontendnone/modifier.go
+++ cmd/copper/web/frontendnone/modifier.go
@@ -1,32 +1,19 @@
 package frontendnone
 
 import (
+	"embed"
 	"github.com/gocopper/cli/pkg/codemod"
 )
 
-func Apply(wd string) error {
-	var indexRoute = codemod.InsertCHTTPRouteParams{
-		Path:        "/",
-		Method:      "Get",
-		HandlerName: "HandleIndex",
-		HandlerBody: `
-	ro.rw.WriteJSON(w, chttp.WriteJSONParams{
-		Data: map[string]string{
-			"message": "Hello, Copper!",
-			"demo": "https://vimeo.com/723537998",
-			"frontend_stack": "none",
-		},
-	})`,
-	}
+//go:embed tmpl/*
+var templatesFS embed.FS
 
+func Apply(wd string) error {
 	return codemod.
 		OpenDir(wd).
+		Apply(codemod.CreateTemplateFiles(templatesFS, nil, true)).
 		OpenFile("./cmd/app/wire.go").
 		Apply(codemod.AddProviderToWireSet("chttp.WireModuleEmptyHTML")).
 		CloseAndOpen("./pkg/app/router.go").
-		Apply(
-			codemod.AddGoImports([]string{"net/http"}),
-			codemod.InsertCHTTPRoute(indexRoute),
-		).
 		CloseAndDone()
 }
